0ca68054fc5a19f0a14b24503cb2ecfabbf0c420
Alex Sears <alexwsears@gmail.com>
2014-03-16 19:28:19 -0400

    Created login functionality and form

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -2,22 +2,24 @@
 
 class HomeController extends BaseController {
 
-	/*
-	|--------------------------------------------------------------------------
-	| Default Home Controller
-	|--------------------------------------------------------------------------
-	|
-	| You may wish to use controllers instead of, or in addition to, Closure
-	| based routes. That's great! Here is an example controller method to
-	| get you started. To route to this controller, just add the route:
-	|
-	|	Route::get('/', 'HomeController@showWelcome');
-	|
-	*/
-
-	public function showWelcome()
+	public function getIndex()
 	{
-		return View::make('hello');
+		return View::make('home.index');
+	}
+
+	public function postIndex()
+	{
+		$username = Input::get('username');
+		$password = Input::get('password');
+
+		if (Auth::attempt(['username' => $username, 'password' => $password]))
+		{
+			return Redirect::intended('/user');
+		}
+
+		return Redirect::back()
+			->withInput()
+			->withErrors('That username/password combo does not exist.');
 	}
 
 }
